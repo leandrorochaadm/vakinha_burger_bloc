2545ad7be49b5d35380687740673390246f382d5
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-02-28 17:57:22 -0300

    SizeExtensions

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vakinha_burger/app/core/config/env/env.dart';
+import 'package:vakinha_burger/app/core/ui/helper/size_extesions.dart';
 import 'package:vakinha_burger/app/core/ui/widgets/widgets.dart';
 
 class SplashPage extends StatelessWidget {
@@ -21,6 +22,28 @@ class SplashPage extends StatelessWidget {
           height: 80,
           onPressed: () {},
         ),
+        Row(
+          children: [
+            SizedBox(
+              width: context.percentWidth(.45),
+              height: context.percentHeight(.1),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Azul'),
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+              ),
+            ),
+            SizedBox(
+              width: context.percentWidth(.55),
+              height: context.percentHeight(.05),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Vermelho'),
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              ),
+            ),
+          ],
+        )
       ]),
     );
   }
