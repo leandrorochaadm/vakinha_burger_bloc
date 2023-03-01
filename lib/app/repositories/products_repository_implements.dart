7e09b8f7547a3d5335432fb6d21ce2f88f3f7937
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-03-01 17:36:41 -0300

    ProductRepositoryImpl

---
+++ lib/app/repositories/products_repository_implements.dart
@@ -0,0 +1,25 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:vakinha_burger/app/core/rest_client/custom_dio.dart';
+import 'package:vakinha_burger/app/models/product_model.dart';
+
+import '../core/exceptions/exceptions.dart';
+import 'products_repository.dart';
+
+class ProductRepositoryImpl implements ProductsRepository {
+  final CustomDio dio;
+
+  ProductRepositoryImpl({required this.dio});
+
+  @override
+  Future<List<ProductModel>> findAllProducts() async {
+    try {
+      final result = await dio.unauth().get('/products');
+      return result.data.map((p) => ProductModel.fromJson(p)).toList();
+    } on DioError catch (e, s) {
+      log("Erro ao buscar produtos", error: e, stackTrace: s);
+      throw RepositoryException(message: "Erro ao buscar produtos");
+    }
+  }
+}
