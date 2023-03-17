73a5ed4c9760a62ccd48a0f294f4b0108cebbff7
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-03-17 14:20:51 -0300

    LoginPage: validadores

--- lib/app/pages/auth/login/login_page.dart
+++ lib/app/pages/auth/login/login_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:vakinha_burger/app/core/ui/styles/text_styles.dart';
+import 'package:validatorless/validatorless.dart';
 
 import '../../../core/ui/widgets/widgets.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  final emailEC = TextEditingController();
+  final passwordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    emailEC.dispose();
+    passwordEC.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,32 +33,47 @@ class LoginPage extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(20.0),
               child: Form(
+                  key: formKey,
                   child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Login',
-                    style: context.textStyles.textTitle,
-                  ),
-                  const SizedBox(height: 30),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'E-mail'),
-                  ),
-                  const SizedBox(height: 30),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Senha'),
-                  ),
-                  const SizedBox(height: 50),
-                  Center(
-                    child: DeliveryButton(
-                      label: 'ENTRAR',
-                      width: double.infinity,
-                      onPressed: () =>
-                          Navigator.pushNamed(context, '/auth/register'),
-                    ),
-                  )
-                ],
-              )),
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Login',
+                        style: context.textStyles.textTitle,
+                      ),
+                      const SizedBox(height: 30),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'E-mail'),
+                        validator: Validatorless.multiple([
+                          Validatorless.required('E-email obrigatório'),
+                          Validatorless.email('Email inválido'),
+                        ]),
+                        controller: emailEC,
+                      ),
+                      const SizedBox(height: 30),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Senha'),
+                        validator:
+                            Validatorless.required('Senha é obrigatória'),
+                        controller: passwordEC,
+                      ),
+                      const SizedBox(height: 50),
+                      Center(
+                        child: DeliveryButton(
+                          label: 'ENTRAR',
+                          width: double.infinity,
+                          onPressed: () {
+                            // Navigator.pushNamed(context, '/auth/register');
+                            final valid =
+                                formKey.currentState?.validate() ?? false;
+                            if (valid) {
+                              //
+                            }
+                          },
+                        ),
+                      )
+                    ],
+                  )),
             ),
           ),
           SliverFillRemaining(
