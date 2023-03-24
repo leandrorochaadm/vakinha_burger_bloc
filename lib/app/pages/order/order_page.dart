ab879e9f933e4e81322ac3cdb9c26f0b86575f71
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-03-23 22:58:54 -0300

    OrderPage - OrderProductTile

--- lib/app/pages/order/order_page.dart
+++ lib/app/pages/order/order_page.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:vakinha_burger/app/core/ui/styles/text_styles.dart';
+import 'package:vakinha_burger/app/core/ui/widgets/delivery_app_bar.dart';
+import 'package:vakinha_burger/app/dto/dto.dart';
+import 'package:vakinha_burger/app/models/models.dart';
+import 'package:vakinha_burger/app/pages/order/widget/order_product_tile.dart';
 
 class OrderPage extends StatelessWidget {
   const OrderPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: DeliveryAppBar(),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: [
+                  Text(
+                    'Carinho',
+                    style: context.textStyles.textTitle,
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Image.asset('assets/images/trashRegular.png')),
+                ],
+              ),
+            ),
+          ),
+          SliverList(
+              delegate:
+                  SliverChildBuilderDelegate(childCount: 2, (context, index) {
+            return Column(
+              children: [
+                OrderProductTile(
+                    index: index,
+                    orderProduct: OrderProductDto(
+                      amount: 10,
+                      product: ProductModel.fromJson({}),
+                    )),
+                const Divider(color: Colors.grey),
+              ],
+            );
+          })),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/app/pages/order/widget/widget.dart
@@ -0,0 +1,1 @@
+export 'order_product_tile.dart';
